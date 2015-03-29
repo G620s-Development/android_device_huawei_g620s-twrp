187ed4f847fc9b73c957b60a94772432539fb71a
EloYGomeZ <eloygomez83@gmail.com>
2015-03-29 23:54:31 +0200

    repair device

--- device.mk
+++ device.mk
@@ -6,15 +6,19 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+# Recovery
+PRODUCT_PACKAGES += \
+    init.recovery.qcom.rc
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/kernel:kernel \
     $(LOCAL_PATH)/dt.img:dt.img \
     $(LOCAL_PATH)/recovery/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/recovery/charger:root/charger \
-    $(LOCAL_PATH)/recovery/sbin/linker:/recovery/root/sbin/linker \
-    $(LOCAL_PATH)/recovery/sbin/rmt_storage:root/sbin/rmt_storage \
-    $(LOCAL_PATH)/recovery/sbin/rmt_oeminfo:root/sbin/rmt_oeminfo \
-    $(LOCAL_PATH)/recovery/sbin/vm_bms:root/sbin/vm_bms \
+    $(LOCAL_PATH)/recovery/sbin/linker:root/sbin/linker \
+    $(LOCAL_PATH)/recovery/sbin/rmt_storage_recovery:root/sbin/rmt_storage_recovery \
+    $(LOCAL_PATH)/recovery/sbin/rmt_oeminfo_recovery:root/sbin/rmt_oeminfo_recovery \
+    $(LOCAL_PATH)/recovery/sbin/vm_bms:root/sbin/vm_bms 
 
 $(call inherit-product, build/target/product/full.mk)
 
